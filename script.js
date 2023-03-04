e0819918d6c31d4017e2a5572c62eb31d5352157
wlee0127 <will.lee.npc@gmail.com>
2023-03-04 11:14:24 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function compute() {
+    p = document.getElementById("principle").value;
+    r = document.getElementById("rate").value;
+    n = document.getElementById("years").value;
+  
+    result = p * r * n;
+    resultPassage = "Your resulting interest in " + n + " years will be $" + result
+  
+    document.getElementById("result").innerHTML = resultPassage;
+  
+  
+  }
+  
+  function myFunction() {
+    var val = document.getElementById("rate").value //gets the input value
+    document.getElementById("rateResult").innerHTML = val //displays this value to the html page
+  }
+  
